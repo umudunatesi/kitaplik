5f35c3012f2ec04b129b65cf58c7c9cce30d5613
ozgurulas <ozgurulascanva@gmail.com>
2025-09-26 23:22:05 +0300

    Deploy Flutter web to GitHub Pages

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"ddf47dd3ff96dbde6d9c614db0d7f019d7c7a2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "770304115"
+    serviceWorkerVersion: "3697289473"
   }
 });
